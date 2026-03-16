d658468ababef02a40146d8d7a710d15afbcdfdb
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-17 00:08:45 +0900

    fix: Update favicon handling to use base64 encoding for improved compatibility

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -1,16 +1,35 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import '@fontsource-variable/inter';
 	import { PUBLIC_CONVEX_URL } from '$env/static/public';
 	import { setupConvex } from 'convex-svelte';
-
 	let { children } = $props();
 	setupConvex(PUBLIC_CONVEX_URL);
+
+	let base64 = "";
+
+	function detectImageTypeFromBase64(base64:string): string {
+  		const firstChar = base64.trim().charAt(3);
+
+		switch (firstChar) {
+ 		  	 case '/': return 'image/jpeg';   // JPEG
+  		 	 case 'i': return 'image/png';    // PNG
+   			 case 'A': return 'image/ico'     // ICO
+    		 case 'P': return 'image/svg+xml';// SVG
+			 case 'U': return 'image/webp';   // WEBP
+			 case 'T': return 'image/tiff';   // TIFF
+			 case 'Q': return 'image/bmp';    // BMP
+			 case 'AAAA': return 'image/avif';    // AVIF
+			 default : return '';     // Default fallback
+		}
+	}
+
+	let faviconUrl = `data:${detectImageTypeFromBase64(base64)};base64,${base64}`;
+
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="{faviconUrl}" />
 </svelte:head>
 
 {@render children?.()}
