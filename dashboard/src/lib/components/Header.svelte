9c21b25d663ad0e490fcf646a844f01bbd23da5c
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-18 07:12:26 +0900

    Refactor Header and Page Components for Consistency and Improved Responsiveness
    - Updated import statements in Header.svelte for consistency in quotation marks.
    - Refactored layout structure in Header.svelte to enhance readability and maintainability.
    - Adjusted responsive classes in +page.svelte and alerts/+page.svelte to improve layout on different screen sizes.
    - Standardized breakpoint values from 770px to 900px for better responsiveness across components.
    - Cleaned up unused code and comments for clarity.

--- dashboard/src/lib/components/Header.svelte
+++ dashboard/src/lib/components/Header.svelte
@@ -1,62 +1,63 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button.svelte';
+  import Button from "$lib/components/ui/button.svelte";
 
-	import { Logo, NotificationMenu, UserMenu } from '$lib/components/_extras/navbars';
-	import { Popover, PopoverTrigger } from '$lib/components/ui/popover';
+  import {
+    Logo,
+    NotificationMenu,
+    UserMenu,
+  } from "$lib/components/_extras/navbars";
+  import { Popover, PopoverTrigger } from "$lib/components/ui/popover";
 
-	// Navigation links array to be used in both desktop and mobile menus
-	// const navigationLinks = [
-	// 	{ href: '#', label: 'Home' },
-	// 	{ href: '#', label: 'Features' },
-	// 	{ href: '#', label: 'Pricing' },
-	// 	{ href: '#', label: 'About' }
-	// ];
-
-	const title = 'Oddin Status - Dashboard';
+  const title = "Oddin Status - Dashboard";
 </script>
 
 <svelte:head>
-	<title>{title}</title>
+  <title>{title}</title>
 </svelte:head>
 
 <header class="border-b px-4 md:px-6">
-	<div class="flex h-16 items-center justify-between gap-4">
-		<!-- Left side -->
-		<div class="flex items-center gap-2">
-			<!-- Mobile menu trigger -->
-			<Popover>
-				<PopoverTrigger>
-					{#snippet child({ props })}
-						<Button class="group size-8 md:hidden" variant="ghost" size="icon" {...props}>
-							<svg
-								class="pointer-events-none"
-								width={16}
-								height={16}
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									d="M4 12L20 12"
-									class="origin-center -translate-y-[7px] transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-315"
-								/>
-								<path
-									d="M4 12H20"
-									class="origin-center transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,0.25,1.8)] group-aria-expanded:rotate-45"
-								/>
-								<path
-									d="M4 12H20"
-									class="origin-center translate-y-[7px] transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-135"
-								/>
-							</svg>
-						</Button>
-					{/snippet}
-				</PopoverTrigger>
-				<!-- <PopoverContent align="start" class="w-36 p-1 md:hidden">
+  <div class="flex h-16 items-center justify-between gap-4">
+    <!-- Left side -->
+    <div class="flex items-center gap-2">
+      <!-- Mobile menu trigger -->
+      <Popover>
+        <PopoverTrigger>
+          {#snippet child({ props })}
+            <Button
+              class="group size-8 md:hidden"
+              variant="ghost"
+              size="icon"
+              {...props}
+            >
+              <svg
+                class="pointer-events-none"
+                width={16}
+                height={16}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 12L20 12"
+                  class="origin-center -translate-y-1.75 transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-315"
+                />
+                <path
+                  d="M4 12H20"
+                  class="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,0.25,1.8)] group-aria-expanded:rotate-45"
+                />
+                <path
+                  d="M4 12H20"
+                  class="origin-center translate-y-1.75 transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-135"
+                />
+              </svg>
+            </Button>
+          {/snippet}
+        </PopoverTrigger>
+        <!-- <PopoverContent align="start" class="w-36 p-1 md:hidden">
 					<NavigationMenuRoot class="max-w-none *:w-full">
 						<NavigationMenuList class="flex-col  items-start gap-0 md:gap-2">
 							{#each navigationLinks as link (link.label)}
@@ -69,22 +70,22 @@
 						</NavigationMenuList>
 					</NavigationMenuRoot>
 				</PopoverContent> -->
-			</Popover>
-			<!-- Main nav  -->
-			<div class="flex items-center gap-6">
-				<a href="/" class="text-primary hover:text-primary/90 hover:opacity-80">
-					<Logo />
-				</a>
-			</div>
-		</div>
-		<!-- Right side -->
-		<div class="flex items-center gap-4">
-			<div class="flex items-center gap-2">
-				<!-- Notification -->
-				<NotificationMenu />
-			</div>
-			<!-- User menu -->
-			<UserMenu />
-		</div>
-	</div>
+      </Popover>
+      <!-- Main nav  -->
+      <div class="flex items-center gap-6">
+        <a href="/" class="text-primary hover:text-primary/90 hover:opacity-80">
+          <Logo />
+        </a>
+      </div>
+    </div>
+    <!-- Right side -->
+    <div class="flex items-center gap-4">
+      <div class="flex items-center gap-2">
+        <!-- Notification -->
+        <NotificationMenu />
+      </div>
+      <!-- User menu -->
+      <UserMenu />
+    </div>
+  </div>
 </header>
--- dashboard/src/routes/alerts/+page.svelte
+++ dashboard/src/routes/alerts/+page.svelte
@@ -28,62 +28,62 @@
 
     <Tabs
       value={currentTab}
-      class="flex max-h-[calc(100dvh-3rem)]  w-full flex-col gap-8 md:flex-row"
+      class="flex max-h-[calc(100dvh-3rem)] w-full flex-col gap-8 900:flex-row"
     >
       <TabsList
-        class="sticky top-6 flex w-80 flex-col gap-2 self-start bg-transparent p-4 max-[770px]:top-0 max-[770px]:z-50 max-[770px]:w-full max-[770px]:flex-row max-[770px]:justify-around max-[770px]:bg-black/80 max-[770px]:backdrop-blur-md"
+        class="sticky top-6 flex w-80 flex-col gap-2 self-start bg-transparent p-4 max-[900px]:top-0 max-[900px]:z-50 max-[900px]:w-full max-[900px]:flex-row max-[900px]:justify-around max-[900px]:bg-black/80 max-[900px]:backdrop-blur-md"
       >
         <TabsTrigger
           value="tab-0"
           onclick={() => goto("/")}
-          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[770px]:justify-center max-[770px]:p-2 xl:px-6 xl:text-3xl"
+          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[900px]:justify-center max-[900px]:p-2 xl:px-6 xl:text-3xl"
         >
           <House class="h-8 w-8" />
-          <span class="max-[770px]:hidden">Home</span>
+          <span class="max-[900px]:hidden">Home</span>
         </TabsTrigger>
 
         <TabsTrigger
           value="tab-1"
           onclick={() => goto("/monitors")}
-          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[770px]:justify-center max-[770px]:p-2 xl:px-6 xl:text-3xl"
+          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[900px]:justify-center max-[900px]:p-2 xl:px-6 xl:text-3xl"
         >
           <SquareActivity class="h-8 w-8" />
-          <span class="max-[770px]:hidden">Monitors</span>
+          <span class="max-[900px]:hidden">Monitors</span>
         </TabsTrigger>
 
         <TabsTrigger
           value="tab-2"
           onclick={() => goto("/incidents")}
-          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[770px]:justify-center max-[770px]:p-2 xl:px-6 xl:text-3xl"
+          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[900px]:justify-center max-[900px]:p-2 xl:px-6 xl:text-3xl"
         >
           <ShieldAlert class="h-8 w-8" />
-          <span class="max-[770px]:hidden">Incidents</span>
+          <span class="max-[900px]:hidden">Incidents</span>
         </TabsTrigger>
 
         <TabsTrigger
           value="tab-3"
           onclick={() => goto("/alerts")}
-          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[770px]:justify-center max-[770px]:p-2 xl:px-6 xl:text-3xl"
+          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[900px]:justify-center max-[900px]:p-2 xl:px-6 xl:text-3xl"
         >
           <Siren class="h-8 w-8" />
-          <span class="max-[770px]:hidden">Alerts</span>
+          <span class="max-[900px]:hidden">Alerts</span>
         </TabsTrigger>
 
         <TabsTrigger
           value="tab-4"
           onclick={() => goto("/schedules")}
-          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[770px]:justify-center max-[770px]:p-2 xl:px-6 xl:text-3xl"
+          class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[900px]:justify-center max-[900px]:p-2 xl:px-6 xl:text-3xl"
         >
           <CalendarCheck class="h-8 w-8" />
-          <span class="max-[770px]:hidden">Schedules</span>
+          <span class="max-[900px]:hidden">Schedules</span>
         </TabsTrigger>
       </TabsList>
 
       <div
-        class="grid w-full grid-cols-1 gap-4 overflow-y-auto px-10 pb-20 md:grid-cols-2"
+        class="grid w-full grid-cols-1 gap-4 overflow-y-auto px-10 pb-20 900:grid-cols-2"
       >
         <div
-          class="relative col-span-1 rounded-lg border border-border bg-zinc-900 p-8 md:col-span-2 md:min-h-120 md:overflow-y-hidden"
+          class="relative col-span-1 rounded-lg border border-border bg-zinc-900 p-8 900:col-span-2 900:min-h-120 900:overflow-y-hidden"
         >
           <TabsContent value="tab-3" class="h-auto min-h-75">
             <Tabs value="tab-3" class=" items-center">
@@ -113,42 +113,25 @@
                 </p>
               </TabsContent>
               <TabsContent value="tab-5">
-                <!-- Card Group -->
-                <div
-                  class="border-card-line mt-5 divide-card-divider grid divide-y overflow-hidden rounded-xl border shadow-2xs sm:flex sm:divide-x sm:divide-y-0"
-                >
-                  <!-- Card -->
+                <div class="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2">
                   <div
-                    class="bg-zinc-800 border-zinc-700 flex h-80 flex-1 p-15 flex-col"
+                    class="flex h-80 w-60 xl:w-80 flex-col overflow-hidden rounded-xl border border-zinc-700 shadow-sm"
                   >
-                    <div class="flex-1 p-4 md:p-5">
-                      <h3 class="text-foreground font-semibold"></h3>
-                      <p class="text-muted-foreground-1 mt-1"></p>
+                    <div class="flex-1 p-6">
+                      <h3 class="font-medium text-zinc-300">Email</h3>
                     </div>
-                    <div class="border-card-footer border-t p-4">
-                      <p class="text-muted-foreground-1 text-xs">
-                        Last updated 5 mins ago
-                      </p>
-                    </div>
+                    <div class="border-t p-4"></div>
                   </div>
-                  <!-- End Group -->
 
-                  <!-- Card -->
                   <div
-                    class="bg-zinc-800 border-zinc-700 flex w-150 h-80 flex-1 p-15 flex-col"
+                    class="flex h-80 w-60 xl:w-80 flex-col overflow-hidden rounded-xl border border-zinc-700 shadow-sm"
                   >
-                    <div class="flex-1 p-4 md:p-5">
-                      <h3 class="text-foreground font-semibold"></h3>
-                      <p class="text-muted-foreground-1 mt-1"></p>
+                    <div class="flex-1 p-6">
+                      <h3 class="font-medium text-zinc-300">RSS</h3>
                     </div>
-                    <div class="border-card-footer border-t p-4">
-                      <p class="text-muted-foreground-1 text-xs">
-                        Last updated 5 mins ago
-                      </p>
-                    </div>
+                    <div class="border-t p-4"></div>
                   </div>
                 </div>
-                <!-- End Card Group -->
               </TabsContent>
               <TabsContent value="tab-6">
                 <p class="p-4 text-center text-xs text-white">
@@ -159,7 +142,7 @@
           </TabsContent>
         </div>
         <div
-          class="min-h-62.5 rounded-lg border border-border bg-zinc-900 p-8 md:overflow-y-hidden"
+          class="min-h-62.5 rounded-lg border border-border bg-zinc-900 p-8 900:overflow-y-hidden"
         >
           <TabsContent value="tab-3" class="h-auto min-h-37.5">
             <p class="text-base font-semibold text-zinc-200">
@@ -169,7 +152,7 @@
         </div>
 
         <div
-          class="min-h-62.5 rounded-lg border border-border bg-zinc-900 p-8 md:overflow-y-hidden"
+          class="min-h-62.5 rounded-lg border border-border bg-zinc-900 p-8 900:overflow-y-hidden"
         >
           <TabsContent value="tab-3" class="h-auto min-h-37.5">
             <p class="text-base font-semibold text-zinc-200">Recent Alerts</p>
