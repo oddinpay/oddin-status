9778fabd800174a1f22a45755e8408ed4d06eac6
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-04-20 04:24:19 +0800

    style: standardize import quotes and improve button click handling in user menu

--- dashboard/src/lib/components/_extras/navbars/user-menu.svelte
+++ dashboard/src/lib/components/_extras/navbars/user-menu.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button.svelte';
+	import Button from "$lib/components/ui/button.svelte";
 
-	import LogOutIcon from '@lucide/svelte/icons/log-out';
-	import { Unplug, BookOpenText } from 'lucide-svelte';
-	import { Avatar, AvatarFallback, AvatarImage } from '$lib/components/ui/avatar';
+	import LogOutIcon from "@lucide/svelte/icons/log-out";
+	import { Unplug, BookOpenText } from "lucide-svelte";
+	import {
+		Avatar,
+		AvatarFallback,
+		AvatarImage,
+	} from "$lib/components/ui/avatar";
 	import {
 		DropdownMenu,
 		DropdownMenuContent,
@@ -11,9 +15,16 @@
 		DropdownMenuItem,
 		DropdownMenuLabel,
 		DropdownMenuSeparator,
-		DropdownMenuTrigger
-	} from '$lib/components/ui/dropdowns';
-	import { goto } from '$app/navigation';
+		DropdownMenuTrigger,
+	} from "$lib/components/ui/dropdowns";
+
+	function handleClick() {
+		window.open(
+			"https://github.com/oddinpay/ohstatus",
+			"_blank",
+			"noreferrer",
+		);
+	}
 </script>
 
 <DropdownMenu>
@@ -33,7 +44,9 @@
 	</DropdownMenuTrigger>
 	<DropdownMenuContent class="max-w-64 bg-zinc-700" align="end">
 		<DropdownMenuLabel class="flex min-w-0  flex-col">
-			<span class="truncate text-sm font-medium text-zinc-100">Keith Kennedy</span>
+			<span class="truncate text-sm font-medium text-zinc-100"
+				>Keith Kennedy</span
+			>
 			<span class="truncate text-xs font-normal text-zinc-300">
 				k.kennedy@originui-svelte.com
 			</span>
@@ -51,13 +64,20 @@
 		</DropdownMenuGroup> -->
 		<DropdownMenuGroup>
 			<DropdownMenuSeparator class="bg-zinc-600" />
-			<DropdownMenuItem class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white">
+			<DropdownMenuItem
+				onclick={() => {
+					handleClick();
+				}}
+				class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white"
+			>
 				<BookOpenText size={16} class="opacity-80" aria-hidden="true" />
 				<span>Docs</span>
 			</DropdownMenuItem>
 		</DropdownMenuGroup>
 		<DropdownMenuSeparator class="bg-zinc-600" />
-		<DropdownMenuItem class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white">
+		<DropdownMenuItem
+			class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white"
+		>
 			<LogOutIcon size={16} class="opacity-80" aria-hidden="true" />
 			<span>Sign out</span>
 		</DropdownMenuItem>
