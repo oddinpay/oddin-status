f8f0829263cd1ae59e9a7872747f6adfbee65e0d
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-04-21 03:51:05 +0800

    fix: truncate time to the nearest second in publishToNATS function

--- api/main.go
+++ api/main.go
@@ -1049,7 +1049,7 @@ func publishToNATS(ctx context.Context, name string, payload *StatusPayload, s *
 		return
 	}
 
-	now := time.Now().UTC()
+	now := time.Now().UTC().Truncate(1 * time.Second)
 
 	// Daily block
 	todayUTC := now.Format("02/01/2006")
