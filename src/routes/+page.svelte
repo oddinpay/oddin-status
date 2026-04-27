5b70941ba7a8d4866c108f84d4646aa11d370050
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-04-28 06:07:31 +0800

    fix: add loading animation for schedules query in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1406,6 +1406,49 @@
                         </div>
                       </div>
                     {:else if i === 1}
+                      {#if schedulesQuery.isLoading}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="48"
+                          height="48"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle cx="18" cy="12" r="0" fill="currentColor">
+                            <animate
+                              attributeName="r"
+                              begin=".67"
+                              calcMode="spline"
+                              dur="1.5s"
+                              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                              repeatCount="indefinite"
+                              values="0;2;0;0"
+                            />
+                          </circle>
+                          <circle cx="12" cy="12" r="0" fill="currentColor">
+                            <animate
+                              attributeName="r"
+                              begin=".33"
+                              calcMode="spline"
+                              dur="1.5s"
+                              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                              repeatCount="indefinite"
+                              values="0;2;0;0"
+                            />
+                          </circle>
+                          <circle cx="6" cy="12" r="0" fill="currentColor">
+                            <animate
+                              attributeName="r"
+                              begin="0"
+                              calcMode="spline"
+                              dur="1.5s"
+                              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                              repeatCount="indefinite"
+                              values="0;2;0;0"
+                            />
+                          </circle>
+                        </svg>
+                      {/if}
+
                       {#if (incidents.length === 0 && maintenances.length === 0) || (!incidents.some( (i) => i.entries.some((e) => e.status === Indicators.Resolved), ) && !maintenances.some( (m) => m.entries.some((e) => e.status === Indicators.Completed || e.status === Indicators.Cancelled), ))}
                         <p
                           class="p-10 text-black text-center"
