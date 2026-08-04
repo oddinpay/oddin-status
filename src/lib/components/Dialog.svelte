c763b1e6437f12b94918b9529c4c33a56aee31ce
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-08-04 18:12:44 +0800

    fix: enhance dialog zoom-in effect for improved visual consistency

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -54,7 +54,7 @@
   </Dialog.Trigger>
 
   <Dialog.Content
-    class="sm:max-w-100 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-75 data-[state=open]:duration-300 data-[state=open]:ease-[cubic-bezier(0.05,0.7,0.1,1.0)] data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-75 data-[state=closed]:duration-150 data-[state=closed]:ease-[cubic-bezier(0.3,0,0.8,0.15)]"
+    class="sm:max-w-100 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:duration-300 data-[state=open]:ease-[cubic-bezier(0.05,0.7,0.1,1.0)] data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:duration-150 data-[state=closed]:ease-[cubic-bezier(0.3,0,0.8,0.15)]"
   >
     {#if sucess}
       <div in:fade={{ duration: 200 }}>
