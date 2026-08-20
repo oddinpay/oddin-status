ea7eaf8fb5b95073abdaa69a7f88a290f0d8ac27
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-08-21 01:23:09 +0800

    feat: add super-sitemap dependency and implement sitemap server route

---
+++ dashboard/src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,13 @@
+import { env } from "$env/dynamic/private";
+import type { RequestHandler } from "@sveltejs/kit";
+import { response } from "super-sitemap/sveltekit";
+
+export const GET: RequestHandler = async () => {
+  const domain = env.DOMAIN || "oddinpay.com";
+
+  const origin = `https://status.${domain}`;
+
+  return await response({
+    origin,
+  });
+};
