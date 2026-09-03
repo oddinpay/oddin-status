a4974a6d5dbab28ae7a24f97aed4595d165e1268
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-09-04 05:37:38 +0800

    fix: update dialog enter and exit animations to use scale for consistency

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -209,22 +209,22 @@
     @keyframes m3-enter {
       from {
         opacity: 0;
-        transform: scale3d(0.95, 0.95, 1) translateZ(0);
+        transform: scale(0.95) translateZ(0);
       }
       to {
         opacity: 1;
-        transform: scale3d(1, 1, 1) translateZ(0);
+        transform: scale(1) translateZ(0);
       }
     }
 
     @keyframes m3-exit {
       from {
         opacity: 1;
-        transform: scale3d(1, 1, 1) translateZ(0);
+        transform: scale(1) translateZ(0);
       }
       to {
         opacity: 0;
-        transform: scale3d(0.95, 0.95, 1) translateZ(0);
+        transform: scale(0.95) translateZ(0);
       }
     }
 
